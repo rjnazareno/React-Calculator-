8614da1cf48c2c05b4279f4c3ea7db40ee1bd6a6
rjnazareno <157678475+rjnazareno@users.noreply.github.com>
2024-04-28 18:19:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('');
+
+    const handleButtonClick = (val) => {
+        setValue(prevValue => prevValue + val);
+    };
+
+    return ( 
+        <div className="container">
+            <div className="calculator">
+                <form action="">
+                    <div className="display">
+                        <input type="text" value={value} readOnly />
+                    </div>
+                    <div>
+                        <input type="button" value="AC" onClick={() => setValue('')} />
+                        <input type="button" value="DE" onClick={() => setValue(value.slice(0, -1))} />
+                        <input type="button" value="." onClick={() => handleButtonClick('.')} />
+                        <input type="button" value="/" onClick={() => handleButtonClick('/')} />
+                    </div>
+                    <div>
+                        <input type="button" value="7" onClick={() => handleButtonClick('7')} />
+                        <input type="button" value="8" onClick={() => handleButtonClick('8')} />
+                        <input type="button" value="9" onClick={() => handleButtonClick('9')} />
+                        <input type="button" value="*" onClick={() => handleButtonClick('*')} />
+                    </div>
+                    <div>
+                        <input type="button" value="4" onClick={() => handleButtonClick('4')} />
+                        <input type="button" value="5" onClick={() => handleButtonClick('5')} />
+                        <input type="button" value="6" onClick={() => handleButtonClick('6')} />
+                        <input type="button" value="-" onClick={() => handleButtonClick('-')} />
+                    </div>
+                    <div>
+                        <input type="button" value="1" onClick={() => handleButtonClick('1')} />
+                        <input type="button" value="2" onClick={() => handleButtonClick('2')} />
+                        <input type="button" value="3" onClick={() => handleButtonClick('3')} />
+                        <input type="button" value="+" onClick={() => handleButtonClick('+')} />
+                    </div>
+                    <div>
+                        <input type="button" value="00" onClick={() => handleButtonClick('00')} />
+                        <input type="button" value="0" onClick={() => handleButtonClick('0')} />
+                        <input type="button" value="=" className='equal' />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
