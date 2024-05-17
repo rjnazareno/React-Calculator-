96f21918df8d647b42c5410c6d22660359b5deff
rjnazareno <157678475+rjnazareno@users.noreply.github.com>
2024-05-18 00:10:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,41 +2,65 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState('0');
 
     const handleButtonClick = (buttonValue) => {
-        if (value === '0' && buttonValue !== '.') {
-       
-            setValue(buttonValue);
-        } else if (buttonValue === 'CLR') {
-
-            setValue('');
-        } else if (buttonValue === 'EQ') {
-            
-            try {
-                const result = eval(value);
-                setValue(result.toString());
-            } catch (error) {
-                setValue('Error');
-            }
-        } else {
-           
-            setValue(value + buttonValue);
+        switch (buttonValue) {
+            case 'CLR':
+                setValue('0');
+                break;
+            case 'EQ':
+                try {
+                    const result = eval(value.replace(/EXP/g, '**').replace(/MOD/g, '%').replace(/DIV/g, '/'));
+                    setValue(result.toString());
+                } catch (error) {
+                    setValue('Error');
+                }
+                break;
+            case 'EXP':
+                setValue(value + '**');
+                break;
+            case 'MOD':
+                setValue(value + '%');
+                break;
+            case 'DIV':
+                setValue(value + '/');
+                break;
+            case 'NEG':
+                if (value !== '0') {
+                    setValue(value.startsWith('-') ? value.slice(1) : '-' + value);
+                }
+                break;
+            default:
+                if (value === '0' && buttonValue !== '.') {
+                    setValue(buttonValue);
+                } else {
+                    setValue(value + buttonValue);
+                }
+                break;
         }
-    };
+    }
 
     return ( 
         <div className="container">
+            <div className="header">
+                <h1>RESTY JAMES M. NAZARENO - CPE 2A</h1>
+            </div>
+
+
             <div className="calculator">
                 <form action="">
                     <div className="display">
                         <input type="text" value={value} readOnly />
                     </div>
                     <div>
-                
+                    <input type="button" value="CLR" onClick={() => handleButtonClick('CLR')} />
+                    <input type="button" value="EXP" onClick={() => handleButtonClick('EXP')} />
+                    <input type="button" value="MOD" onClick={() => handleButtonClick('MOD')} />
+                    <input type="button" value="DIV" onClick={() => handleButtonClick('DIV')} />
                     </div>
                     <div>
-                        <input type="button" value="7" onClick={() => handleButtonClick('7')} />
+                    <input type="button" value="7" onClick={() => handleButtonClick('7')} />
                         <input type="button" value="8" onClick={() => handleButtonClick('8')} />
                         <input type="button" value="9" onClick={() => handleButtonClick('9')} />
                         <input type="button" value="MUL" onClick={() => handleButtonClick(' * ')} />
@@ -54,9 +78,9 @@ function App() {
                         <input type="button" value="ADD" onClick={() => handleButtonClick(' + ')} />
                     </div>
                     <div>
-                        <input type="button" value="CLR" onClick={() => handleButtonClick('CLR')} />
+                        <input type="button" value="NEG" onClick={() => handleButtonClick('NEG')} />
                         <input type="button" value="0" onClick={() => handleButtonClick('0')} />
-                        <input type="button" value="DIV" onClick={() => handleButtonClick(' / ')} />
+                        <input type="button" value="." onClick={() => handleButtonClick(' . ')} />
                         <input type="button" value="EQ" className='EQ' onClick={() => handleButtonClick('EQ')} />
                     </div>
                 </form>
